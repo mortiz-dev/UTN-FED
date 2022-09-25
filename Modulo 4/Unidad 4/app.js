d3e76fee34e79c6629cae98bd40a4aaf18bbcf32
Miguel <mortiz@nebuluxs.com>
2022-09-25 15:13:21 -0300

    Instalacion mysql y util

--- Modulo 4/Unidad 4/app.js
+++ Modulo 4/Unidad 4/app.js
@@ -6,10 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pool = require('./models/db');
 
 var app = express();
 
 require('dotenv').config();
+
+var loginRouter = require('./routes/admin/login');
+app.use('/admin/login', loginRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
