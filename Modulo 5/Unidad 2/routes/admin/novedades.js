e9f9342ce8a1f4307e69f020e0ac43e676389240
Miguel <mortiz@nebuluxs.com>
2022-10-14 12:12:37 -0300

    Arreglo en layout y Servicios

--- Modulo 5/Unidad 2/routes/admin/novedades.js
+++ Modulo 5/Unidad 2/routes/admin/novedades.js
@@ -2,14 +2,7 @@ var express = require('express');
 var router = express.Router();
 var serviciosModel = require('./../../models/serviciosModel');
 
-router.get('/', function(req, res, next) {
-    res.render('admin/novedades',{
-        layout: 'admin/layout',
-        usuario: req.session.nombre,
-    });
-});
-
-router.get('/servicios', async function(req, res, next) {
+router.get('/', async function(req, res, next) {
     var servicios = await serviciosModel.getServicios();
     res.render('admin/novedades',{
         layout: 'admin/layout',
